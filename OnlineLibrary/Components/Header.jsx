dc2275d9b6b9d4c4443f12a57418dd09bccdc665
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-06-07 23:24:50 +0530

    added navigation

--- OnlineLibrary/Components/Header.jsx
+++ OnlineLibrary/Components/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return(
         <ul className="Nav">
-            <Link to="/"><li>Home</li></Link>
-            <Link to="/Search"><li>BrowseBooks</li></Link>
-            <Link to="/AddNew"><li>AddBook</li></Link>
+            <Link to="/"><li className="Nav-link">Home</li></Link>
+            <Link to="/Search"><li className="Nav-link">BrowseBooks</li></Link>
+            <Link to="/AddNew"><li className="Nav-link">AddBook</li></Link>
         </ul>
     )
 }
