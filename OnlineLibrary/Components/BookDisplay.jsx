1c5de0349631a8eee8162d56c10a13a2fa64df10
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-06-08 20:25:21 +0530

    Styled Book description page

--- OnlineLibrary/Components/BookDisplay.jsx
+++ OnlineLibrary/Components/BookDisplay.jsx
@@ -1,12 +1,11 @@
-import Bookdesc from "./Bookdesc"
 import { Link } from "react-router-dom"
 export default function BookDisplay({book}){
     return(
         <Link to={`/book/${book.id}`}>
-        <div className="Book-container">
-        <div className="Book-detail">
-        <h1 className="Book-title">{book.title}</h1>
-        <img src={`${book.cover_img}`} alt={`${book.id} book`} className="Book-img" />
+        <div className="display-container">
+        <div className="display-detail">
+        <h1 className="display-title">{book.title}</h1>
+        <img src={`${book.cover_img}`} alt={`${book.id} book`} className="display-img" />
         </div>
         </div>
         </Link>
--- OnlineLibrary/Components/Bookdesc.jsx
+++ OnlineLibrary/Components/Bookdesc.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import { mockBooks } from "../utils/books-details"
+import { Link } from "react-router-dom";
 export default function Bookdesc(){
      const { id } = useParams(); // Get book ID from URL
     const book = mockBooks.find(b => b.id === parseInt(id)); // Find book by ID
@@ -9,14 +10,16 @@ export default function Bookdesc(){
 return(
      <div className="Book-container">
         <div className="Book-detail">
-        <h1 className="Book-title">{book.title}</h1>
         <img src={`${book.cover_img}`} alt={`${book.id} book`} className="Book-img" />
-        
+        <div className="Detail-text">
+        <h1 className="Book-title">{book.title}</h1>
         <h2 className="Book-author"> <span className="span-text">By:</span> {book.author}</h2>
         <p className="Book-description"><span className="span-text">Description: </span>{book.description}</p>
         <div className="mini-desc">
-            <p className="rating"><span className="span-text">Rating: </span>{book.rating}</p>
-             <p className="genre"><span className="span-text">Genre: </span>{book.genre}</p>
+            <p className="end-desc"><span className="span-text">Rating: </span>{book.rating}</p>
+             <p className="end-desc"><span className="span-text">Genre: </span>{book.genre}</p>
+             </div>
+             <Link to="/Search"><button className="Search-btn">Go to Book</button></Link>
         </div>
         
         </div>
